c6478ff8fc40620afef726b041ee839d30ce63b0
Vinicius Felicissimo <viniciusfc95@gmail.com>
2017-07-08 14:03:09 -0300

    Inclusão do ELectreII

---
+++ tc2/main.m
@@ -0,0 +1,21 @@
+ClusterDBCSV = 'ClusterDB.csv';
+EquipDBCSV = 'EquipDB.csv';
+MPDBCSV = 'MPDB.csv';
+
+% Parâmetros
+horizonte_tempo = 5;
+
+T = csvread(MPDBCSV);
+custo_manutencao = T(:,3)';
+fator_risco = T(:,2)';
+
+T = csvread(EquipDBCSV);
+t0 = T(:,2)';
+cluster = T(:,3)';
+custo_falha = T(:,4)';
+
+T = csvread(ClusterDBCSV);
+eta = T(:,2)';
+beta = T(:,3)';
+
+
